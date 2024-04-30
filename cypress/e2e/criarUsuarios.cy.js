bb71196c9f3780355e58813533c7aafa1d117ed6
Gabriel Vilela <gabrielvcr98@gmail.com>
2024-04-30 18:26:44 -0300

    new

---
+++ cypress/e2e/criarUsuarios.cy.js
@@ -0,0 +1,3 @@
+describe ("Criação de usuários", () => {
+    it ("Deve conseguir cadastrar usuário")
+})
---
+++ cypress/e2e/listarUsuarios.cy.js
@@ -0,0 +1,8 @@
+describe ("Listar usuários", () => {
+  it ("Deve exibir todas as informações dos usuários cadastrados", () => {
+
+  })
+  it ("Deve possuir mais de uma página quando possuir mais de 6 usuários cadastrados", () =>{
+
+  })
+})
